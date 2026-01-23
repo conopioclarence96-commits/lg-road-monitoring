f3063aa7c20489740fa9770a95fdcae2554902d9
conopioclarence96-commits <conopioclarence96@gmail.com>
2026-01-23 17:37:46 +0800

    a

--- road_and_infra_dept/config/database.local.php
+++ road_and_infra_dept/config/database.local.php
@@ -7,7 +7,7 @@
  */
 return [
     'host' => 'localhost',        // e.g., 'localhost' or an IP address
-    'username' => 'root',         // your database username
+    'username' => 'rgmap_gu_road_infra',         // your database username
     'password' => '',             // your database password
     'database' => 'rgmap_gu_road_infra' // your database name
 ];
