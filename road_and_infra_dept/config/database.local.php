4861e3c5ba626766d1857a196759dccf57b267c1
clarence conopio <conopioclarence96@gmail.com>
2026-01-30 02:16:29 +0800

    Update database.local.php

--- road_and_infra_dept/config/database.local.php
+++ road_and_infra_dept/config/database.local.php
@@ -4,10 +4,16 @@
  * 
  * Replace the values below with your live server database credentials.
  * This file is loaded by database.php and will override any defaults.
+ * 
+ * COMMON MYSQL CONFIGURATIONS:
+ * 1. XAMPP: username='root', password='', database='your_db_name'
+ * 2. WAMP: username='root', password='', database='your_db_name'  
+ * 3. MAMP: username='root', password='root', database='your_db_name'
+ * 4. Custom: username='your_username', password='your_password', database='your_db_name'
  */
 return [
     'host' => 'localhost',        // e.g., 'localhost' or an IP address
     'username' => 'root',         // your database username
-    'password' => '',             // your database password
+    'password' => '',             // your database password - TRY EMPTY FIRST
     'database' => 'rgmap_gu_road_infra' // your database name
 ];
