f1f08e87c43680ef94d83c0d18e105f6af638c2b
clarence conopio <conopioclarence96@gmail.com>
2026-03-02 14:42:10 +0800

    Update sidebar.php

--- lgu_staff/includes/sidebar.php
+++ lgu_staff/includes/sidebar.php
@@ -339,6 +339,21 @@ $notification_count = getNotificationCount();
         </div>
 
         <div class="sidebar-content">
+            <!-- Home Button -->
+            <div class="nav-section">
+                <ul style="list-style: none;">
+                    <li>
+                        <a href="../../../index.php" class="nav-link" target="_parent">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-6V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5V7.5h10z"/>
+                                <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                            </svg>
+                            Home
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            
             <?php 
             // Debug output
             error_log("User role: " . $user_role);
