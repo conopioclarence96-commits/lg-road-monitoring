835789f525f25ac2fb9c67e91e1701cba63aeaad
clarence conopio <conopioclarence96@gmail.com>
2026-03-05 18:05:52 +0800

    a

--- lgu_staff/pages/monitoring/verification_monitoring.php
+++ lgu_staff/pages/monitoring/verification_monitoring.php
@@ -305,8 +305,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif;
+        }
+
         body {
-        background: url("../../../assets/img/cityhall.jpeg") center/cover no-repeat fixed;
+            background: url("../../../assets/img/cityhall.jpeg") center/cover no-repeat fixed;
             position: relative;
             min-height: 100vh;
             margin: 0;
