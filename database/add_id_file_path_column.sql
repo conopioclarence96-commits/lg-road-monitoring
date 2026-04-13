f1dc2c419da87b1c7a8fc978e02c7891a1081172
clarence conopio <conopioclarence96@gmail.com>
2026-04-13 14:57:04 +0800

    a

--- database/add_id_file_path_column.sql
+++
@@ -1,5 +0,0 @@
--- Add id_file_path column to users table for storing uploaded ID file paths
--- Run this SQL to update the users table structure
-
-ALTER TABLE `users`
-ADD COLUMN `id_file_path` VARCHAR(255) NULL AFTER `civil_status`;
--- database/update_users_table.sql
+++
@@ -1,7 +0,0 @@
--- Add new columns to users table
--- Run this SQL to update the users table structure
-
-ALTER TABLE `users`
-ADD COLUMN `address` VARCHAR(100) NULL AFTER `department`,
-ADD COLUMN `birthday` DATE NULL AFTER `address`,
-ADD COLUMN `civil_status` VARCHAR(50) NULL AFTER `birthday`;
