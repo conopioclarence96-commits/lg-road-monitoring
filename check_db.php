c8d81ab6d72b5ffdf562fc70f3f2fac094a4209f
renz <conopioclarence96@gmail.com>
2026-07-15 00:44:59 +0800

    removing the files that are no function

--- check_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'lgu_staff/includes/config.php';
-$result = $conn->query('DESCRIBE users');
-while($row = $result->fetch_assoc()) {
-    echo $row['Field'] . ' - ' . $row['Type'] . PHP_EOL;
-}
-?>
