9c7323a9842097310e37f73e3fb8adb489b931d7
renz <conopioclarence96@gmail.com>
2026-09-11 17:31:44 +0800

    change now applies across all 14 public landing-page pages

--- includes/hamburger_menu.php
+++ includes/hamburger_menu.php
@@ -40,9 +40,8 @@ $__hm_is_index = (basename($_SERVER['PHP_SELF'] ?? '') === 'index.php');
             <li><a href="<?php echo $__hm_base; ?>road-updates.php"><i class="fas fa-newspaper"></i> Road Updates</a></li>
             <li><a href="<?php echo $__hm_base; ?>public_reports.php"><i class="fas fa-map-marked-alt"></i> Road Status</a></li>
             <li><a href="<?php echo $__hm_base; ?>infrastructure_projects.php"><i class="fas fa-hard-hat"></i> Infrastructure Projects</a></li>
-            <?php if ($__hm_is_index): ?>
-                <li class="menu-divider" aria-hidden="true"></li>
-            <?php endif; ?>
+            <li class="menu-divider" aria-hidden="true"></li>
+            <li><a href="<?php echo $__hm_is_index ? '#public-transport' : $__hm_base . 'transportation-updates.php'; ?>"><i class="fas fa-bus"></i> Transportation</a></li>
             <li><a href="<?php echo $__hm_base; ?>transportation-updates.php"><i class="fas fa-bus"></i> Transportation Updates</a></li>
             <li><a href="<?php echo $__hm_base; ?>transportation-status.php"><i class="fas fa-traffic-light"></i> Transportation Status</a></li>
             <li><a href="<?php echo $__hm_base; ?>public_transparency_view.php"><i class="fas fa-balance-scale"></i> Transparency</a></li>
